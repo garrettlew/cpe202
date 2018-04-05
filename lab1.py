99dfa1c58ffeadeb6351b2501ef46c4823e95956
Garrett Lew <garrett.r.lew@gmail.com>
2018-04-05 11:09:37 -0700

    Added bin_search

--- lab1.py
+++ lab1.py
@@ -2,7 +2,7 @@
 def max_list_iter(int_list):  # must use iteration not recursion
     """finds the max of a list of numbers and returns the value (not the index)
     If int_list is empty, returns None. If list is None, raises ValueError"""
-    
+
     if len(int_list) == 0:
         return None
     if int_list == None:
@@ -19,23 +19,45 @@ def reverse_rec(int_list):   # must use recursion
     If list is None, raises ValueError"""
 
     # Base Case
-    if len(int_list) == 0:
-        return None
     if int_list == None:
         raise ValueError
-    # Base Case
-    if len(int_list) == 1:
+    # Base Case list is empty or has 1 element
+    if len(int_list) <= 1:
         return int_list
-    # Base Case
+    # Base Case Swap
     elif len(int_list) == 2:
         return [ int_list[1], int_list[0] ]
     else:
-        # Interative
-        temp = int_list[1:]
-        return reverse_rec(temp) + [int_list[0]]
+    # Recursive Step
+        return reverse_rec( int_list[1:] ) + [int_list[0]]
 
 
 
 def bin_search(target, low, high, int_list):  # must use recursion
     """searches for target in int_list[low..high] and returns index if found
     If target is not found returns None. If list is None, raises ValueError """
+
+    # Base case
+    if int_list == None:
+        raise ValueError
+    # Base Case where target not found
+    if high < low or target not in int_list:
+        return None
+    # Base Case where target is at the middle index
+    mid = high + low // 2
+
+    if int_list[mid] == target:
+        return mid
+
+    # Recursive Step
+    if target in int_list[low:mid]:    # If target on bottom half of list
+        bin_search(target, low, mid-1, int_list)
+    else:    # Target on top half of list
+        bin_search(target, mid+1, high, int_list)
+
+
+
+
+
+
+
